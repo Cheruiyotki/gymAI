baaa4fd1fcf9744bca56ce4e4e2a420c4238c584
Cheruiyotki <wilbornkipkoech@gmail.com>
2026-06-17 13:49:34 +0300

    backend

--- server/src/routes/plan.ts
+++ server/src/routes/plan.ts
@@ -1,13 +1,20 @@
 import { Router, type Request, type Response } from "express";
+import { error } from "node:console";
 
 export const planRouter = Router();
 
 
-planRouter.post("/", async (req: Request, res: Response) => {
+planRouter.post("/generate", async (req: Request, res: Response) => {
     try {
+        const {userId} = req.body;
+
+        if (!userId) {
+            return res.status(400).json({error: "User ID is required"});
+        }
 
     } catch (error) {
-        
+       console.error("Error generating plan:", error);
+       res.status(500).json({error: "Failed to generate plan" });
     }
 
 })
