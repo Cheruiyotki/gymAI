77eda64e07689aa3c326e46f732e33e04869c695
Cheruiyotki <wilbornkipkoech@gmail.com>
2026-06-17 15:19:12 +0300

    backend

---
+++ server/src/lib/ai.ts
@@ -0,0 +1,9 @@
+import OpenAI from "openai";
+import dotenv from "dotenv"
+import { UserProfile } from "../../types";
+
+dotenv.config();
+
+export async function generateTrainingPlan(profile:  UserProfile) {
+
+}
---
+++ server/types/index.ts
@@ -0,0 +1,12 @@
+
+
+
+export interface UserProfile {
+  goal: string;
+  experience: string;
+  days_per_week: number;
+  session_length: number;
+  equipment: string;
+  injuries?: string | null;
+  preferred_split: string;
+}
